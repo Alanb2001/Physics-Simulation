c31e35de786b0349f59522e667005aee94ba3730
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2021-11-16 10:51:11 +0000

    Sphere to sphere detection redone part 4

--- Assets/Scripts/Sphere Collisions/SphereToSphereSolver.cs
+++ Assets/Scripts/Sphere Collisions/SphereToSphereSolver.cs
@@ -6,35 +6,48 @@ namespace Sphere_Collisions
 {
     public class SphereToSphereSolver : MonoBehaviour
     {
-        public GameObject sphere;
-        public GameObject sphere2;
+        private GameObject[] spheres;
+
+        private void Start()
+        {
+            spheres = GameObject.FindGameObjectsWithTag("Sphere");
+        }
 
         private bool SphereDetection()
         {
-            Vector3 p1 = sphere.transform.position;
-            Vector3 p2 = sphere2.transform.position;
-            float r1 = sphere.GetComponent<MeshRenderer>().bounds.extents.magnitude;
-            float r2 = sphere2.GetComponent<MeshRenderer>().bounds.extents.magnitude;
-            Vector3 v1 = sphere.GetComponent<StartingVelocity>().velocity;
-            Vector3 v2 = sphere2.GetComponent<StartingVelocity>().velocity;
-            float a = ((v1 - v2) * Time.deltaTime).sqrMagnitude;
-            float b = Vector3.Dot(p1 - p2, (v1 - v2) * Time.deltaTime) * 2;
-            float c = (p1 - p2).sqrMagnitude - (r1 + r2) * (r1 + r2);
-            float t = (-b - Mathf.Sqrt(b * b - 4 * a * c)) / (2 * a);
-
-            //print(t);
-            
-            if (t < 1 && t > 0)
+            for (int i = 0; i < spheres.Length; i++)
             {
-                print("Hit");
-                return true;
+                for (int j = 0; j < spheres.Length; j++)
+                {
+                    Vector3 p1 = spheres[j].transform.position;
+                    Vector3 p2 = spheres[i].transform.position;
+                    float r1 = spheres[j].GetComponent<MeshRenderer>().bounds.extents.magnitude;
+                    float r2 = spheres[i].GetComponent<MeshRenderer>().bounds.extents.magnitude;
+                    Vector3 v1 = spheres[j].GetComponent<StartingVelocity>().velocity;
+                    Vector3 v2 = spheres[i].GetComponent<StartingVelocity>().velocity;
+                    float a = ((v1 - v2) * Time.deltaTime).sqrMagnitude;
+                    float b = Vector3.Dot(p1 - p2, (v1 - v2) * Time.deltaTime) * 2;
+                    float c = (p1 - p2).sqrMagnitude - (r1 + r2) * (r1 + r2);
+                    float t = (-b - Mathf.Sqrt(b * b - 4 * a * c)) / (2 * a);
+
+                    if (t < 1 && t > 0)
+                    {
+                        print(t);
+                        print("Hit");
+                        return true;
+                    }
+                }
+
             }
-            
+
+            //print(spheres.Length);
+
             //float q = Vector3.Angle(v, a);
             //float d = Mathf.Sin(q) + a.magnitude;
 
             //float e = Mathf.Sqrt(r1 + r2 * 2 - d * 2);
             //float vc = Mathf.Cos(q) + a.magnitude - e;
+
             //vc = vc + e + Mathf.Cos(q) + a.magnitude;
 
             //if (d < r1 + r2)
